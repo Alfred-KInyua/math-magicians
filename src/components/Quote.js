81cf2b72bbd9e3364fc716da938f1f9d455e645e
Kinyua <alfkinyua@gmail.com>
2022-08-09 13:11:58 +0300

    Add style

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Quotes = () => (
   <main>
     <div className="qoute">
-      <p>
+      <p className="pquote">
         Mathematics is not about numbers, equations, computations or
         algorithms; it&apos;s about understanding. - William Paul Thurston
       </p>
