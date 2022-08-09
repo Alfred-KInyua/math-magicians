fec4c02227c4a2c2772998bce0722b46d2a04bc3
Kinyua <alfkinyua@gmail.com>
2022-08-09 09:47:13 +0300

    add pages

---
+++ src/components/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Home = () => (
+  <main>
+    <div className="title">
+      <h2>Welcome to our pages!</h2>
+    </div>
+    <div className="description">
+      <div className="text">
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+        </p>
+        <p>
+          amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi
+        </p>
+        <p>
+          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit
+        </p>
+      </div>
+      <div className="text">
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+        </p>
+        <p>
+          amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi
+        </p>
+        <p>
+          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit
+        </p>
+      </div>
+    </div>
+  </main>
+);
+
+export default Home;
---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Quotes = () => (
+  <main>
+    <div className="qoute flex align-center justify-center">
+      <p>
+        Mathematics is not about numbers, equations, computations or
+        algorithms; it&apos;s about understanding. - William Paul Thurston
+      </p>
+    </div>
+  </main>
+);
+
+export default Quotes;
